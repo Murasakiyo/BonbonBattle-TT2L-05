2375fc2075d498edf17445608746a9420ce65e3e
Dane Alfonzo <danielzainee04@gmail.com>
2024-04-16 10:31:05 +0800

    Tried out some test codes

--- Test codes/try.py
+++ Test codes/try.py
@@ -22,9 +22,15 @@ def FollowMe(pops, fpos):
 
     return (new_follower_vector.x, new_follower_vector.y) 
 
+
+
 pygame.init()
 window = pygame.display.set_mode((500, 500))
 clock = pygame.time.Clock()
+
+lines = [((20, 300), (150, 20)), ((250, 20), (380, 250)), ((50, 350), (350, 300))] 
+rect = pygame.Rect(180, 180, 40, 40)
+speed = 5
 
 follower = (100, 100)
 run = True
@@ -37,9 +43,23 @@ while run:
     player   = pygame.mouse.get_pos()
     follower = FollowMe(player, follower)
 
+    keys = pygame.key.get_pressed()
+    rect.x += (keys[pygame.K_d] - keys[pygame.K_a]) * speed
+    rect.y += (keys[pygame.K_s] - keys[pygame.K_w]) * speed
+
+    if any(rect.clipline(*line) for line in lines):
+        color = "red"
+
+    else:
+        color = "green"
+
+
     window.fill(0)  
-    pygame.draw.circle(window, (0, 0, 255), player, 10)
+    pygame.draw.circle(window, color, player, 10)
     pygame.draw.circle(window, (255, 0, 0), (round(follower[0]), round(follower[1])), 10)
+    pygame.draw.rect(window, color, rect)
+    for line in lines:
+        pygame.draw.line(window, "white", *line)
     pygame.display.flip()
 
 pygame.quit()
