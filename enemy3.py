ba5bb82a3781a154827c4a339100e3642b60364d
Dane Alfonzo <danielzainee04@gmail.com>
2024-04-22 22:25:00 +0800

    testing enemy3 codes

--- enemy3.py
+++ enemy3.py
@@ -2,14 +2,18 @@ import pygame
 import time
 import math
 import spritesheet
+import state
+import torres
 
 class Enemy3(pygame.sprite.Sprite):
     def __init__(self, game, group):
         # super().__init__(group)
         self.game = game
+        self.camera = state.CameraGroup(self.game)
+        self.player = torres.Player(self.game, self.camera)
         self.rect_draw = pygame.Rect(180, 180, 40, 40)
         self.color = "white"
-        self.speed = 5
+        self.speed = 1
 
 
     def update(self, deltatime, player_action):
@@ -19,14 +23,14 @@ class Enemy3(pygame.sprite.Sprite):
         # self.rect_draw.x += 400 * deltatime * direction_x 
         # self.rect_draw.y += 450 * deltatime * direction_y 
 
-        self.move_towards_player(player_action)
+        self.move_towards_player()
 
     def render(self, display):
         pygame.draw.rect(display, self.color, self.rect_draw)
 
-    def move_towards_player(self, player):
+    def move_towards_player(self):
         # Find direction vector (dx, dy) between enemy and player.
-        dx, dy = player.rect.x - self.rect_draw.x, player.rect.y - self.rect_draw.y
+        dx, dy = self.player.rect.x - self.rect_draw.x, self.player.rect.y - self.rect_draw.y
         dist = math.hypot(dx, dy)
         dx, dy = dx / dist, dy / dist  # Normalize.
         # Move along this normalized vector towards the player at current speed.
