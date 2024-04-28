5a1c710128c0b4ded4fdc4f5e3cd835a8b11158a
Yaro Murasaki <yaroyace@gmail.com>
2024-04-28 18:50:45 +0800

    Added comments and put variables/loops into functions

--- stage.py
+++ stage.py
@@ -26,10 +26,6 @@ class Stage(State):
         self.ultimate = False
         self.countdown = 0
         self.immunity = False
-        self.init_stan = False
-        self.init_louie = False
-        self.init_krie = False
-
 
     def update(self, deltatime, player_action):
 
@@ -46,34 +42,18 @@ class Stage(State):
             # Update player
             self.player.update(deltatime, player_action)
 
+            # Sprite group update
             for support in self.support_dolls.sprites():
                 support.update(deltatime, player_action, self.player.rect.x, self.player.rect.y)
 
             if self.game.ult_finish == False:
+                # Check for collision rect and the mask collision
                 if pygame.sprite.spritecollide(self.player, self.confection_ult, False):
                     if pygame.sprite.spritecollide(self.player, self.confection_ult, False, pygame.sprite.collide_mask):
-                        self.confection_ult.empty()
+                        self.confection_ult.empty() # Remove everything in the sprite group
 
-            if self.init_louie == False and self.init_krie == False:
-                if pygame.sprite.spritecollide(self.player, self.vanilla_grp, False, pygame.sprite.collide_mask):
-                    self.init_stan = True
-                    self.camera.add(self.stan)
-            if self.init_stan == False and self.init_krie == False:
-                if pygame.sprite.spritecollide(self.player, self.float_grp, False, pygame.sprite.collide_mask):
-                    self.init_louie = True
-                    self.camera.add(self.louie)
-            if self.init_louie == False and self.init_stan == False:
-                if pygame.sprite.spritecollide(self.player, self.strawb_grp, False, pygame.sprite.collide_mask):
-                    self.init_krie = True
-                    self.camera.add(self.krie)
-
-            if self.init_stan:
-                self.support_dolls.add(self.stan)
-            if self.init_louie:
-                self.support_dolls.add(self.louie)
-            if self.init_krie:
-                self.support_dolls.add(self.krie)
-
+            self.check_specifics()
+           
         if player_action["ultimate"]:
             self.game.ult = True
 
@@ -88,21 +68,7 @@ class Stage(State):
                 self.torres_ult.update(deltatime,player_action)
 
         if self.game.ult_finish:
-            self.init_stan = False
-            self.init_louie = False
-            self.init_krie = False
-
-            for support in self.support_dolls.sprites():
-                support.rect.x, support.rect.y = 0,200
-
-            self.confection_ult.add(self.vanilla)
-            self.confection_ult.add(self.float)
-            self.confection_ult.add(self.strawb)
-
-            self.stan.kill()
-            self.louie.kill()
-            self.krie.kill()
-            self.game.ult_finish = False
+            self.ultimate_reset()
 
 
 
@@ -135,21 +101,26 @@ class Stage(State):
         self.stan = Stanley(self.game) 
         self.krie = Krie(self.game)
 
-        # self.support_dolls.add(self.louie)
-        # self.support_dolls.add(self.stan)
-        # self.support_dolls.add(self.krie)
+        # Initiation of a specific support doll
+        self.init_stan = False
+        self.init_louie = False
+        self.init_krie = False
+
 
     
-
+    # All sprites, ultimate sprite groups, and non-sprite ultimates are here
     def ultimates(self):
+
         self.vanilla_grp = pygame.sprite.Group()
         self.float_grp = pygame.sprite.Group()
         self.strawb_grp = pygame.sprite.Group()
+
         self.torres_ult = Torres_Ult(self.game)
         self.stan_ult = Stan_Ult(self.game)
         self.louie_ult = Louie_Ult(self.game)
         self.krie_ult = Krie_Ult(self.game)
 
+        # Confection drops--------------------------------
         self.vanilla = Vanilla(self.game, self.vanilla_grp)
         self.float = Float(self.game, self.float_grp)
         self.strawb = Strawb(self.game, self.strawb_grp)
@@ -157,7 +128,51 @@ class Stage(State):
         self.confection_ult.add(self.vanilla)
         self.confection_ult.add(self.float)
         self.confection_ult.add(self.strawb)
+        # -------------------------------------------------
 
+    # Reset all sprite groups and sprite position
+    def ultimate_reset(self):
+        self.init_stan = False
+        self.init_louie = False
+        self.init_krie = False
+
+        for support in self.support_dolls.sprites():
+            support.rect.x, support.rect.y = 0,200
+
+        self.confection_ult.add(self.vanilla)
+        self.confection_ult.add(self.float)
+        self.confection_ult.add(self.strawb)
+
+        self.stan.kill()
+        self.louie.kill()
+        self.krie.kill()
+        self.game.ult_finish = False
+
+
+    # Check the specific confection that is picked up
+    def check_specifics(self):
+
+        # Enable ultimate initiation and adding sprites into sprite groups
+        if self.init_louie == False and self.init_krie == False:
+            if pygame.sprite.spritecollide(self.player, self.vanilla_grp, False, pygame.sprite.collide_mask):
+                self.init_stan = True
+                self.camera.add(self.stan)
+        if self.init_stan == False and self.init_krie == False:
+            if pygame.sprite.spritecollide(self.player, self.float_grp, False, pygame.sprite.collide_mask):
+                self.init_louie = True
+                self.camera.add(self.louie)
+        if self.init_louie == False and self.init_stan == False:
+            if pygame.sprite.spritecollide(self.player, self.strawb_grp, False, pygame.sprite.collide_mask):
+                self.init_krie = True
+                self.camera.add(self.krie)
+
+        # Add sprites into support doll sprite grp for UPDATES
+        if self.init_stan:
+            self.support_dolls.add(self.stan)
+        if self.init_louie:
+            self.support_dolls.add(self.louie)
+        if self.init_krie:
+            self.support_dolls.add(self.krie)
 
 
 
