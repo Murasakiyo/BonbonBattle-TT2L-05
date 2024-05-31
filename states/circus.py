2c6fdfb69a13ac1c2f4416d5b46d5ca6a5020034
Yaro Murasaki <yaroyace@gmail.com>
2024-06-01 02:41:45 +0800

    Adjusted circus room to prepare for shop

--- states/circus.py
+++ states/circus.py
@@ -10,12 +10,16 @@ class Circus(State, Dialogue):
         self.game = game
         self.player = Player(self.game, 300, 320)
         self.krie = Krie(self.game)
+        self.krie.rect.x, self.krie.rect.y = 800, 250
         self.camera = CameraGroup(self.game)
         self.camera.add(self.player)
         self.door_rect = pygame.Rect(170,300,100,200)
         self.button_e = self.game.E_button
+        self.button_shop = self.game.E_button
         self.button_rect = self.button_e.get_rect()
+        self.shop_rect = self.button_shop.get_rect()
         self.button_rect.x, self.button_rect.y = self.door_rect.centerx - 20, self.door_rect.y - 60
+        self.shop_rect.x, self.shop_rect.y = self.krie.rect.centerx - 35, self.krie.rect.y - 40
 
 
 
@@ -25,7 +29,6 @@ class Circus(State, Dialogue):
         self.player.update(deltatime,player_action)
 
         self.krie.update_circus(deltatime, self.player, player_action)
-        self.krie.rect.x, self.krie.rect.y = 800, 250
 
         if self.player.rect.colliderect(self.door_rect):
             if player_action["E"]:
@@ -45,6 +48,9 @@ class Circus(State, Dialogue):
         self.camera.custom_draw(display)
         if self.player.rect.colliderect(self.door_rect):
             display.blit(self.button_e, self.button_rect)
+        
+        if self.player.rect.colliderect(self.krie.rect):
+            display.blit(self.button_shop, self.shop_rect)
 
         display.blit(self.game.sugarcube_image, (10, 10))
         self.game.draw_text(display, f"{int(self.game.current_currency)}", (30,30,30), 40, 10, 35)
